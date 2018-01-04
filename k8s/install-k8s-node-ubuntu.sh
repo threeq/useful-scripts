a316d4cd8ec13c51a7d896c29706842a8d9eea6d
three <448987046@qq.com>
2018-01-04 17:25:40 +0800

    脚本

--- k8s/install-k8s-node-ubuntu.sh
+++ k8s/install-k8s-node-ubuntu.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+fail() {
+	echo "$*"
+	exit
+}
+
+echo "install k8s node on ubuntu"
+echo
+echo
+
 join_args=$1
 
 #####################################
 
 echo "staring install docker"
 
+apt-get remove docker docker-engine docker.io
+apt-get autoremove -y
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common || fail ""
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
@@ -16,7 +27,14 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 
 echo "install docker version $(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')"
-apt-get install docker-compose docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+# apt-get install -y docker-compose docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+apt-get install -y docker-compose docker-ce
+
+docker version || fail "docker install error" 
+
+
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://2f19a520.m.daocloud.io
+systemctl restart docker
 
 echo "install docker end"
 
@@ -31,6 +49,8 @@ EOF
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3746C208A7317B0F
 
 apt-get update && apt-get install -y kubeadm
+
+kubeadm --version || fail "kubeadm install error"
 
 echo "install <kubeadm> end"
 
@@ -49,6 +69,31 @@ docker pull quay.io/coreos/flannel:v0.9.1-amd64
 
 #####################################
 
+swapoff -a
+
+cat > /etc/sysctl.d/k8s.conf << EOF
+net.bridge.bridge-nf-call-ip6tables=1
+net.bridge.bridge-nf-call-iptables=1
+vm.swappiness=0
+EOF
+
+sysctl -p /etc/sysctl.d/k8s.conf
+
+# xxx
+systemctl stop firewalld
+systemctl disable firewalld
+
+systemctl stop ufw
+systemctl disable ufw
+
+setenforce 0
+mkdir -p /etc/selinux
+cat > /etc/selinux/config << EOF
+SELINUX=disabled
+EOF
+
+#####################################
+
 echo "join k8s cluster"
 
-kubeadm join $join_args
+#kubeadm join $join_args
